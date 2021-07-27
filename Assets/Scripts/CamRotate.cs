938523bfcce96606bb5c38b5e7f580ee0194aaf9
zaranara <87846679+zaranara@users.noreply.github.com>
2021-07-27 09:24:59 +0900

    카메라 조정

--- Assets/Scripts/CamRotate.cs
+++ Assets/Scripts/CamRotate.cs
@@ -11,6 +11,9 @@ public class CamRotate : MonoBehaviour
     // 회전 값 변수
     float mx = 0;
     float my = 0;
+
+    public float low_angleLimit = 30.0f;
+    public float high_angleLimit = 30.0f;
     
     void Start()
     {
@@ -29,8 +32,8 @@ public class CamRotate : MonoBehaviour
         mx += mouse_X * rotSpeed * Time.deltaTime;
         my += mouse_Y * rotSpeed * Time.deltaTime;
 
-        // 1-2. 마우스 상하 이동 회전 변수(my) 값을 -90 ~ 90도 사이로 제한
-        my = Mathf.Clamp(my, -90f, 90f);
+        // 1-2. 마우스 상하 이동 회전 변수(my) 값 제한
+        my = Mathf.Clamp(my, -low_angleLimit, high_angleLimit);
 
         // 2. 마우스 입력 값을 이용해 회전 방향 결정
         transform.eulerAngles = new Vector3(-my, mx, 0);

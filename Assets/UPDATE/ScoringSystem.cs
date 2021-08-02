0c20c1080c1ec3f83889818af82c6a53142588af
HJ2828 <shhihi01@gmail.com>
2021-08-02 14:55:13 +0900

    ScoringSystem 스크립트 수정

--- Assets/UPDATE/ScoringSystem.cs
+++ Assets/UPDATE/ScoringSystem.cs
@@ -8,6 +8,11 @@ public class ScoringSystem : MonoBehaviour
     public GameObject scoreText;
     public static int theScore;
 
+    private void Start()
+    {
+        theScore = 0;
+    }
+
     void Update()
     {
         // 백신 얻으면 UI의 개수 1증가
